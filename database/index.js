640c5dccb7abe4877a664be89ee2129a0d6255ab
Saurabh Ojha <107386248+Saurabh-ojha-2000@users.noreply.github.com>
2024-07-06 18:21:34 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = () => {
    
-    mongoose.connect('mongodb+srv://sourabhojha12:saurabh@cluster0.ej0hfdh.mongodb.net/dvd?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect('mongodb+srv://sourabhojha12:saurabh@cluster0.6rb2olc.mongodb.net/dvd?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => {
         console.log("Database connected successfully...");
     })
